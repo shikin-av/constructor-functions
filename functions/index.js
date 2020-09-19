34e643a0cf7cd42a808aead7c00dee7b6a474aa3
alexandr shikin <shikin.a.v@mail.ru>
2020-09-19 15:59:49 +0400

    load blueprint

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,8 @@ exports.saveBlueprint = functions.https.onCall(async (data, context) => {
   // TODO: вместо userId спользовать uid
   const { userId, id, blueprint } = data
 
+  const blueprintId = id || new Date().toISOString()
+
   if (!userId) return Promise.reject(new Error('doesn`t have userId'))
   if (!blueprint) return Promise.reject(new Error('doesn`t have blueprint'))  
 
@@ -21,32 +23,59 @@ exports.saveBlueprint = functions.https.onCall(async (data, context) => {
   console.log('id = ', id)
   console.log('blueprint = ', blueprint)
   console.log('=======================================')
-
-  const blueprintId = id || new Date().toISOString()
   
   // TODO: set(JSON.parse({ blueprint, imageUrl })
-  await db.collection(`blueprints/users/${userId}`).doc(blueprintId).set(JSON.parse(blueprint))
-
-  return Promise.resolve(JSON.stringify({ blueprintId }))
+  try {
+    await db.collection(`blueprints/users/${userId}`).doc(blueprintId).set(JSON.parse(blueprint))
+    return Promise.resolve(JSON.stringify({ blueprintId }))
+  } catch(e) {
+    return Promise.reject(new Error(`can't save blueprint ${id} - ${e}`))
+  }
 })
 
 exports.loadBlueprintsPage = functions.https.onCall(async (data, context) => {
   // TODO: вместо userId спользовать uid
   const { userId, startAt, limit } = data
 
+  if (!userId) return Promise.reject(new Error('doesn`t have userId'))
   if (isNaN(startAt)) return Promise.reject(new Error('startAt is NaN'))
   if (isNaN(limit)) return Promise.reject(new Error('limit is NaN'))
 
-  const snapshot = await db.collection(`blueprints/users/${userId}`).get()
-  const blueprints = snapshot.docs.map(doc => ({ 
-    id: doc.id,
-  }))
-  // TODO - вернуть мобилке только id-шники и только стартуя с startAt с количеством limit
-  // TODO - вместе с id возвращать doc.data().imageUrl
+  try {
+    const snapshot = await db.collection(`blueprints/users/${userId}`).get()
+    const blueprints = snapshot.docs.map(doc => ({ 
+      id: doc.id,
+    }))
 
-  console.log('=======================================')
-  console.log('BLUEPRINTS = ', JSON.stringify({ blueprints }))
-  console.log('=======================================')
+    // TODO - вернуть мобилке только id-шники и только стартуя с startAt с количеством limit
+    // TODO - вместе с id возвращать doc.data().imageUrl
 
-  return Promise.resolve(JSON.stringify({ blueprints }))
+    console.log('=======================================')
+    console.log('BLUEPRINTS = ', JSON.stringify({ blueprints }))
+    console.log('=======================================')
+
+    return Promise.resolve(JSON.stringify({ blueprints }))
+  } catch(e) {
+    return Promise.reject(new Error(`can't load blueprint page with userId:${userId}, startAt:${startAt}, limit:${limit} - ${e}`))
+  }
+})
+
+exports.loadBlueprint = functions.https.onCall(async (data, context) => {
+  const { userId, id } = data
+
+  if (!userId) return Promise.reject(new Error('doesn`t have userId'))
+  if (!id) return Promise.reject(new Error('doesn`t have id'))
+
+  try {
+    const doc = await db.collection(`blueprints/users/${userId}`).doc(id).get()
+    
+    if (!doc.exists) {
+      return Promise.reject(new Error(`No such document! ${id}`))
+    } else {
+      const blueprint = doc.data();
+      return Promise.resolve(JSON.stringify(blueprint))
+    }
+  } catch (e) {
+    return Promise.reject(new Error(`can't load blueprint ${id} - ${e}`))
+  }  
 })

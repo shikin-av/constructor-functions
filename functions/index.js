4224f81ff374effaf9b39e07fe76f3e8faf4b43b
alexandr shikin <shikin.a.v@mail.ru>
2020-09-19 12:54:33 +0400

    load blueprints - all yet

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,8 @@ admin.initializeApp()
 const db = admin.firestore();
 
 exports.saveBlueprint = functions.https.onCall(async (data, context) => {
-  const { userId, id, blueprint } = data  // TODO: вместо userId спользовать uid
+  // TODO: вместо userId спользовать uid
+  const { userId, id, blueprint } = data
 
   if (!userId) return Promise.reject(new Error('doesn`t have userId'))
   if (!blueprint) return Promise.reject(new Error('doesn`t have blueprint'))  
@@ -23,7 +24,29 @@ exports.saveBlueprint = functions.https.onCall(async (data, context) => {
 
   const blueprintId = id || new Date().toISOString()
   
-  await db.collection(`blueprints/users/${userId}`).doc(blueprintId).set(JSON.parse(blueprint))   // TODO: вместо userId спользовать uid
+  // TODO: set(JSON.parse({ blueprint, imageUrl })
+  await db.collection(`blueprints/users/${userId}`).doc(blueprintId).set(JSON.parse(blueprint))
 
   return Promise.resolve(JSON.stringify({ blueprintId }))
+})
+
+exports.loadBlueprintsPage = functions.https.onCall(async (data, context) => {
+  // TODO: вместо userId спользовать uid
+  const { userId, startAt, limit } = data
+
+  if (isNaN(startAt)) return Promise.reject(new Error('startAt is NaN'))
+  if (isNaN(limit)) return Promise.reject(new Error('limit is NaN'))
+
+  const snapshot = await db.collection(`blueprints/users/${userId}`).get()
+  const blueprints = snapshot.docs.map(doc => ({ 
+    id: doc.id,
+  }))
+  // TODO - вернуть мобилке только id-шники и только стартуя с startAt с количеством limit
+  // TODO - вместе с id возвращать doc.data().imageUrl
+
+  console.log('=======================================')
+  console.log('BLUEPRINTS = ', JSON.stringify({ blueprints }))
+  console.log('=======================================')
+
+  return Promise.resolve(JSON.stringify({ blueprints }))
 })
